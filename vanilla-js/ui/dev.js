63a4c587a7f1de51ad00ad64235d10e6b9282536
Breze <39080460+brezedc@users.noreply.github.com>
2022-11-15 20:08:18 +0100

    Devmode file.

---
+++ vanilla-js/ui/dev.js
@@ -0,0 +1,21 @@
+window.addEventListener("load", () => {
+    if (window.invokeNative) {
+        const phoneWrapper = document.getElementById("phone-wrapper");
+        const app = phoneWrapper.querySelector(".app");
+        phoneWrapper.parentNode.insertBefore(app, phoneWrapper);
+        phoneWrapper.parentNode.removeChild(phoneWrapper);
+        return;
+    };
+
+    document.getElementById("phone-wrapper").style.display = "block";
+    document.body.style.visibility = "visible";
+
+    function center() {
+        document.getElementById("phone-wrapper").style.scale = window.innerWidth/1920;
+        document.getElementById("phone-wrapper").style.marginTop = window.innerHeight/2 - 960/2 + "px";
+        document.getElementById("phone-wrapper").style.marginLeft = window.innerWidth/2 - 540/2 + "px";
+    }
+    
+    center();
+    window.addEventListener("resize", center);
+})
