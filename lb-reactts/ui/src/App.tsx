528e38a448ed923b37267e32899a9b0af41e39f6
Loaf <60279520+loaf-scripts@users.noreply.github.com>
2025-01-23 16:06:24 +0100

    reactts: use new components system, add types for components, rescale dev frame on resize

--- lb-reactts/ui/src/App.tsx
+++ lb-reactts/ui/src/App.tsx
@@ -1,52 +1,35 @@
-import React, { useEffect, useRef, useState } from 'react';
-import './App.css';
-import Frame from './components/Frame';
+import { ReactNode, useEffect, useRef, useState } from 'react'
+import './App.css'
+import Frame from './components/Frame'
 
-const devMode = !window?.['invokeNative'];
+const devMode = !window?.['invokeNative']
 
 const App = () => {
-    const [theme, setTheme] = useState('light');
-    const [direction, setDirection] = useState('N');
-    const [notificationText, setNotificationText] = useState('Notification text');
-    const appDiv = useRef(null);
+    const [gameRender, setGameRender] = useState(true)
+    const [notificationText, setNotificationText] = useState('Notification text')
 
-    const { setPopUp, setContextMenu, selectGIF, selectGallery, selectEmoji, fetchNui, sendNotification, getSettings, onSettingsChange, colorPicker, useCamera } = window as any;
+    const appDiv = useRef(null)
 
     useEffect(() => {
         if (devMode) {
-            document.getElementsByTagName('html')[0].style.visibility = 'visible';
-            document.getElementsByTagName('body')[0].style.visibility = 'visible';
-            return;
-        } else {
-            getSettings().then((settings: any) => setTheme(settings.display.theme));
-            onSettingsChange((settings: any) => setTheme(settings.display.theme));
+            document.body.style.visibility = 'visible'
+            return
         }
-
-        fetchNui('getDirection').then((direction: string) => setDirection(direction));
-
-        window.addEventListener('message', (e) => {
-            if (e.data?.type === 'updateDirection') setDirection(e.data.direction);
-        });
-    }, []);
+    }, [])
 
     useEffect(() => {
-        if (notificationText === '') setNotificationText('Notification text');
-    }, [notificationText]);
+        if (notificationText === '') setNotificationText('Notification text')
+    }, [notificationText])
 
     return (
         <AppProvider>
-            <div className='app' ref={appDiv} data-theme={theme}>
-                <div className='app-wrapper'>
-                    <div className='header'>
-                        <div className='title'>Custom App Template</div>
-                        <div className='subtitle'>React TS</div>
-                        <a className='subtitle'>{direction}</a>
-                    </div>
-                    <div className='button-wrapper'>
+            <div className="app" ref={appDiv}>
+                <div className="app-wrapper">
+                    <Header />
+                    <div className="button-wrapper">
                         <button
-                            id='button'
                             onClick={() => {
-                                setPopUp({
+                                components.setPopUp({
                                     title: 'Popup Menu',
                                     description: 'Confirm your choice',
                                     buttons: [
@@ -54,130 +37,135 @@ const App = () => {
                                             title: 'Cancel',
                                             color: 'red',
                                             cb: () => {
-                                                console.log('Cancel');
+                                                console.log('Cancel')
                                             }
                                         },
                                         {
                                             title: 'Confirm',
                                             color: 'blue',
                                             cb: () => {
-                                                console.log('Confirm');
+                                                console.log('Confirm')
                                             }
                                         }
                                     ]
-                                });
+                                })
                             }}
                         >
                             Popup Menu
                         </button>
                         <button
-                            id='context'
                             onClick={() => {
-                                setContextMenu({
+                                components.setContextMenu({
                                     title: 'Context menu',
                                     buttons: [
                                         {
                                             title: 'Phone Notification',
                                             color: 'blue',
                                             cb: () => {
-                                                sendNotification({ title: notificationText });
+                                                sendNotification({ title: notificationText })
                                             }
                                         },
                                         {
                                             title: 'GTA Notification',
                                             color: 'red',
                                             cb: () => {
-                                                fetchNui('drawNotification', { message: notificationText });
+                                                fetchNui('drawNotification', { message: notificationText })
                                             }
                                         }
                                     ]
-                                });
+                                })
                             }}
                         >
                             Context menu
                         </button>
                         <button
-                            id='gif'
                             onClick={() => {
-                                selectGIF((gif: string) => {
-                                    setPopUp({
-                                        title: 'Selected GIF',
-                                        attachment: { src: gif },
-                                        buttons: [
-                                            {
-                                                title: 'OK'
-                                            }
-                                        ]
-                                    });
-                                });
-                            }}
-                        >
-                            Gif Selector
-                        </button>
-                        <button
-                            id='gallery'
-                            onClick={() => {
-                                selectGallery({
-                                    includeVideos: true,
-                                    includeImages: true,
-                                    cb: (data: { src: string; isVideo: boolean; timestamp: number }) => {
-                                        setPopUp({
-                                            title: 'Selected media',
-                                            attachment: data,
+                                components.setGifPickerVisible({
+                                    onSelect(gif) {
+                                        components.setPopUp({
+                                            title: 'Selected GIF',
+                                            attachment: { src: gif },
                                             buttons: [
                                                 {
                                                     title: 'OK'
                                                 }
                                             ]
-                                        });
+                                        })
                                     }
-                                });
+                                })
+                            }}
+                        >
+                            Gif Selector
+                        </button>
+                        <button
+                            onClick={() => {
+                                components.setGallery({
+                                    includeVideos: true,
+                                    includeImages: true,
+                                    allowExternal: true,
+                                    multiSelect: false,
+
+                                    onSelect(data) {
+                                        components.setPopUp({
+                                            title: 'Selected media',
+                                            attachment: { src: Array.isArray(data) ? data[0].src : data.src },
+                                            buttons: [
+                                                {
+                                                    title: 'OK'
+                                                }
+                                            ]
+                                        })
+                                    }
+                                })
                             }}
                         >
                             Gallery Selector
                         </button>
                         <button
-                            id='emoji'
                             onClick={() => {
-                                selectEmoji((emoji: string) => {
-                                    setPopUp({
-                                        title: 'Selected emoji',
-                                        description: emoji,
-                                        buttons: [
-                                            {
-                                                title: 'OK'
-                                            }
-                                        ]
-                                    });
-                                });
+                                components.setEmojiPickerVisible({
+                                    onSelect: (emoji) => {
+                                        components.setEmojiPickerVisible(false)
+                                        components.setPopUp({
+                                            title: 'Selected emoji',
+                                            description: emoji.emoji,
+                                            buttons: [
+                                                {
+                                                    title: 'OK'
+                                                }
+                                            ]
+                                        })
+                                    }
+                                })
                             }}
                         >
                             Emoji Selector
                         </button>
                         <button
-                            id='colorpicker'
                             onClick={() => {
-                                colorPicker((color: string) => {
-                                    setPopUp({
-                                        title: 'Selected color',
-                                        description: color,
-                                        buttons: [
-                                            {
-                                                title: 'OK'
-                                            }
-                                        ]
-                                    });
-                                });
+                                components.setColorPicker({
+                                    onSelect(color) {},
+                                    onClose(color) {
+                                        components.setPopUp({
+                                            title: 'Selected color',
+                                            description: color,
+                                            buttons: [
+                                                {
+                                                    title: 'OK'
+                                                }
+                                            ]
+                                        })
+                                    }
+                                })
                             }}
                         >
                             Color Picker
                         </button>
                         <button
-                            id='camreacomponent'
                             onClick={() => {
                                 useCamera(
                                     (url) => {
-                                        setPopUp({
+                                        components.setPopUp({
                                             title: 'Media taken',
                                             attachment: { src: url },
                                             buttons: [
@@ -185,7 +173,7 @@ const App = () => {
                                                     title: 'OK'
                                                 }
                                             ]
-                                        });
+                                        })
                                     },
                                     {
                                         default: {
@@ -201,27 +189,175 @@ const App = () => {
                                             takeLandscapePhoto: true
                                         }
                                     }
-                                );
+                                )
                             }}
                         >
                             Camera Component
                         </button>
-                        <input placeholder='Notification text' onChange={(e: any) => setNotificationText(e.target.value)}></input>
+                        <button
+                            onClick={() => {
+                                setGameRender((prev) => !prev)
+                            }}
+                        >
+                            Game render
+                        </button>
+                        {gameRender && (
+                            <GameRender
+                                remove={(url, blob) => {
+                                    setGameRender(false)
+
+                                    if (url) {
+                                        components.setPopUp({
+                                            title: 'Photo taken',
+                                            attachment: { src: url },
+                                            buttons: [
+                                                {
+                                                    title: 'Close',
+                                                    color: 'red'
+                                                },
+                                                {
+                                                    title: 'Save',
+                                                    color: 'blue',
+                                                    cb: async () => {
+                                                        const url = await components.uploadMedia('Image', blob)
+
+                                                        if (!url) return
+
+                                                        components.saveToGallery(url)
+                                                    }
+                                                }
+                                            ]
+                                        })
+                                    }
+                                }}
+                            />
+                        )}
+                        <input placeholder="Notification text" onChange={(e: any) => setNotificationText(e.target.value)}></input>
                     </div>
                 </div>
             </div>
         </AppProvider>
-    );
-};
+    )
+}
 
-const AppProvider = ({ children }: { children: React.ReactNode }) => {
+const Header = () => {
+    const [direction, setDirection] = useState('N')
+
+    useEffect(() => {
+        if (devMode) return
+
+        fetchNui<string>('getDirection').then(setDirection)
+        useNuiEvent('updateDirection', setDirection)
+    }, [])
+
+    return (
+        <div className="header">
+            <div className="title">Custom App Template</div>
+            <div className="subtitle">React TS</div>
+            <a className="subtitle">{direction}</a>
+        </div>
+    )
+}
+
+const GameRender = ({ remove }: { remove: (photo?: string, blob?: Blob) => void }) => {
+    const aspectRatio = 9 / 18
+
+    const canvasRef = useRef<HTMLCanvasElement>(null)
+    const gameRenderRef = useRef<ReturnType<typeof components.createGameRender>>(null)
+
+    useEffect(() => {
+        if (!canvasRef.current) return
+
+        const gameRender = components.createGameRender(canvasRef.current)
+
+        gameRenderRef.current = gameRender
+
+        gameRender.resizeByAspect(aspectRatio)
+
+        fetchNui('toggleCamera', true)
+
+        const checkDestroyedInterval = setInterval(() => {
+            if (gameRender.destroyed) remove()
+        }, 1000)
+
+        return () => {
+            fetchNui('toggleCamera', false)
+
+            if (checkDestroyedInterval) clearInterval(checkDestroyedInterval)
+
+            gameRenderRef.current?.destroy()
+        }
+    }, [canvasRef.current])
+
+    return (
+        <div
+            className="gamerender-blur"
+            onClick={() => {
+                remove()
+            }}
+        >
+            <div
+                className="gamerender-container"
+                style={{
+                    aspectRatio: aspectRatio
+                }}
+                onClick={(event) => {
+                    event.stopPropagation()
+
+                    const gameRender = gameRenderRef.current
+
+                    if (!gameRender || gameRender.destroyed) return
+
+                    gameRender.takePhoto().then((blob) => {
+                        const url = URL.createObjectURL(blob)
+
+                        remove(url, blob)
+                    })
+                }}
+            >
+                <canvas
+                    style={{
+                        width: '100%',
+                        height: '100%'
+                    }}
+                    ref={canvasRef}
+                />
+            </div>
+        </div>
+    )
+}
+
+const AppProvider = ({ children }: { children: ReactNode }) => {
     if (devMode) {
+        const handleResize = () => {
+            const { innerWidth, innerHeight } = window
+
+            const aspectRatio = innerWidth / innerHeight
+            const phoneAspectRatio = 27.6 / 59
+
+            if (phoneAspectRatio < aspectRatio) {
+                document.documentElement.style.fontSize = '1.66vh'
+            } else {
+                document.documentElement.style.fontSize = '3.4vw'
+            }
+        }
+
+        useEffect(() => {
+            window.addEventListener('resize', handleResize)
+
+            return () => {
+                window.removeEventListener('resize', handleResize)
+            }
+        }, [])
+
+        handleResize()
+
         return (
-            <div className='dev-wrapper'>
+            <div className="dev-wrapper">
                 <Frame>{children}</Frame>
             </div>
-        );
-    } else return children;
-};
+        )
+    } else return children
+}
 
-export default App;
+export default App
---
+++ lb-reactts/ui/src/components.d.ts
@@ -0,0 +1,311 @@
+interface GameRender {
+    quality: number
+    xOffset: number
+    yOffset: number
+    recorder: MediaRecorder | undefined
+    recording: boolean
+    destroyed: boolean
+    canvas: HTMLCanvasElement | null
+    gl: WebGLRenderingContext | null
+    program: WebGLProgram | null
+    paused: boolean
+    viewport: {
+        x: number
+        y: number
+        width: number
+        height: number
+    }
+    canvasSize: {
+        width: number
+        height: number
+    }
+    animationFrame: number | null
+    mainTexture: WebGLTexture | null
+    hasPrevFrame: boolean
+    prevTextures: WebGLTexture[]
+    prevBuffers: WebGLFramebuffer[]
+
+    pause(): void
+    resume(): void
+    resize(width: number, height: number): void
+    resizeByAspect(ratio: number): void
+    setQuality(quality: number): void
+    setXOffset(offset: number): void
+    setYOffset(offset: number): void
+    destroy(keepCanvas?: boolean): void
+    takePhoto(): Promise<Blob>
+    startRecording(cb: (blob: Blob) => void): MediaRecorder | undefined
+    render(): void
+}
+
+type ContactsData = {
+    number: string
+    name?: string
+    firstname?: string
+    lastname?: string
+    email?: string
+    address?: string
+    favourite?: boolean
+    blocked?: boolean
+    avatar?: string
+    company?: string
+    from?: string
+}
+
+type PopUp = {
+    title: string
+    description?: string
+    vertical?: boolean
+
+    inputs?: PopUpInput[]
+    input?: PopUpInput
+
+    contact?: ContactsData
+    attachment?: {
+        src: string
+    }
+    buttons: {
+        title: string
+        cb?: () => void
+        disabled?: boolean
+        bold?: boolean
+
+        color?: 'red' | 'blue'
+    }[]
+}
+
+type PopUpInput = Partial<HTMLInputElement> & {
+    minCharacters?: number
+    maxCharacters?: number
+    onChange?: (value: string) => void
+}
+
+type CameraComponentData = {
+    default?: {
+        type?: 'Photo' | 'Video' | 'Landscape'
+        flash?: boolean
+        camera?: 'rear' | 'front'
+    }
+    permissions?: {
+        toggleFlash?: boolean
+        flipCamera?: boolean
+        takePhoto?: boolean
+        takeVideo?: boolean
+        takeLandscapePhoto?: boolean
+    }
+    saveToGallery?: boolean
+}
+
+type MusicSelectorData = {
+    onSelect: (data: { id: string; src: string; title: string; artist: string; album: string }) => void
+    searchQuery?: string
+    artist?: string
+    album?: string
+}
+
+type Contextmenu = {
+    title?: string
+    buttons: {
+        title: string
+        color?: 'red' | 'blue'
+        disabled?: boolean
+        cb?: () => void
+    }[]
+}
+
+type PhotoData = {
+    id: number
+    src: string
+    timestamp?: number
+
+    type?: string
+    favourite?: boolean
+    isVideo?: boolean
+
+    size?: number
+
+    duration?: number
+}
+
+type GalleryData = {
+    includeVideos?: boolean
+    includeImages?: boolean
+    allowExternal?: boolean
+    multiSelect?: boolean
+
+    onCancel?: () => void
+    onSelect: (data: PhotoData | PhotoData[]) => void
+}
+
+type GifSelectorData = {
+    onSelect: (gif: string) => void
+}
+
+type SwitchProps = {
+    disabled?: boolean
+    theme?: 'light' | 'dark'
+    checked?: boolean
+    defaultChecked?: boolean
+    onChange?: () => void
+}
+
+type FullscreenImagedata = {
+    display: boolean
+    image?: string
+}
+
+type ColorPickerData = {
+    customApp?: boolean
+    defaultColor?: string
+    onSelect: (color: string) => void
+    onClose?: (color: string) => void
+}
+
+type ShareComponentData = {
+    type: 'image' | 'contact' | 'location' | 'note' | 'voicememo'
+    data?: {
+        // image
+        isVideo?: boolean
+        src?: string
+
+        // contact
+        number?: string
+        firstname?: string
+        lastname?: string
+        avatar?: string
+        email?: string
+        address?: string
+
+        //location
+        name?: string
+        location?: unknown
+
+        //note
+        title?: string
+        content?: string
+        timestamp?: number
+
+        //voicememo
+        duration?: number
+
+        // timestamp?: number;
+        // src?: string;
+    }
+}
+
+type ContactSelectorData = {
+    onSelect: (contact: ContactsData) => void
+    filter?: string[]
+    options?: {
+        allowPhoneNumber?: boolean
+    }
+}
+
+type Settings = {
+    airplaneMode: boolean
+    streamerMode: boolean
+    doNotDisturb: boolean
+    locale: string
+
+    name: string
+    avatar?: string
+    address?: string
+    email?: string
+
+    display: {
+        brightness: number
+        size: number
+        theme: 'dark' | 'light'
+        automatic: boolean
+        frameColor?: string
+    }
+    security: {
+        pinCode: boolean
+        faceId: boolean
+    }
+    wallpaper: {
+        background: string
+        blur?: boolean
+    }
+    time: {
+        twelveHourClock: boolean
+    }
+    sound: {
+        volume: number
+        ringtone: string
+        texttone: string
+        silent: boolean
+    }
+    weather: {
+        celcius: boolean
+    }
+    storage: {
+        used: number
+        total: number
+    }
+    phone: {
+        showCallerId: boolean
+    }
+    notifications?: {
+        [key: string]: {
+            enabled: boolean
+            sound: boolean
+        }
+    }
+    lockscreen: {
+        color: string
+        fontStyle: number
+        layout: number
+    }
+    apps: string[][]
+
+    version?: string
+    latestVersion?: string
+}
+
+declare global {
+    var components: {
+        createGameRender: (canvas: HTMLCanvasElement) => GameRender
+        uploadMedia: (uploadType: 'Video' | 'Image' | 'Audio', blob: Blob) => Promise<string>
+        saveToGallery: (url: string, size?: number, type?: 'screenshot' | 'selfie' | 'import', shouldLog?: boolean) => Promise<unknown>
+        fetchPhone: (eventName: string, data?: unknown, mockData?: unknown) => Promise<unknown>
+        setColorPicker: (data: ColorPickerData) => void
+        setPopUp: (data: PopUp) => void
+        setContextMenu: (data: Contextmenu) => void
+        setMusicSelector: (data: MusicSelectorData) => void
+        setContactSelector: (data: ContactSelectorData) => void
+        setEmojiPickerVisible: (
+            data?:
+                | false
+                | {
+                      onSelect: (emoji?: { activeSkinTone?: string; emoji?: string; names?: string[] }) => void
+                  }
+        ) => void
+        setShareComponent: (data: ShareComponentData) => void
+        setGifPickerVisible: (data: GifSelectorData) => void
+        setGallery: (data: GalleryData) => void
+        setFullscreenImage: (data: FullscreenImagedata) => void
+        setHomeIndicatorVisible: (visible: boolean) => void
+    }
+    var fetchNui: <T>(eventName: string, data?: unknown, mockData?: T) => Promise<T>
+    var useNuiEvent: <T>(eventName: string, cb: (data: T) => void) => void
+    var formatPhoneNumber: (number: string) => string
+    var getSettings: () => Promise<Settings>
+    var onSettingsChange: (cb: (settings: Settings) => void) => void
+    var setApp: (app: string | { name: string; data: any }) => void
+    var createCall: (options: { number?: string; videoCall?: boolean; company?: string; hideNumber?: boolean }) => void
+    var sendNotification: (data: {
+        title: string
+        content?: string
+        thumbnail?: string
+        avatar?: string
+        showAvatar?: boolean
+        source?: number
+    }) => void
+    var useCamera: (cb: (url: string) => void, options: CameraComponentData) => void
+    var settings: Settings
+    var appName: string
+    var resourceName: string
+}
+
+export {}
