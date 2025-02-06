11e003e0de1346730a40d84f11055f27cca1cb28
Greve <frankgrevekrogh@gmail.com>
2025-02-06 22:25:47 +0100

    re

--- lb-reactts/config.lua
+++
@@ -1,8 +0,0 @@
-Config = {}
-
-Config.Identifier = "react-ts-template"
-Config.DefaultApp = true
-
-Config.Name = "React TS"
-Config.Description = "Template app using React and TypeScript."
-Config.Developer = "LB Scripts"
